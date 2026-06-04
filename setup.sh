de6410d7a5ebb2e0c2568f4f9f357fa9b68ea97e
Aiyaz Mostofa <aiyaz5040@gmail.com>
2026-06-04 15:20:29 -0500

    Bug with making symlink inside .dotfiles

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 sudo usermod --shell /usr/bin/fish "$USER"
 sudo mkdir -p /var/nix
 sudo chown -v -R "$USER:$USER" /var/nix
-ln -s "$(brew --repository aiyazmostofa/dotfiles)" ~/.dotfiles
+if [ ! -e "$HOME/.dotfiles" ]; then
+    ln -s "$(brew --repository aiyazmostofa/dotfiles)" "$HOME/.dotfiles"
+fi
 ./sync.sh
