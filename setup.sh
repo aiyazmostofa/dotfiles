29b3d0dcb4430050d3198f7b6dfccf951768ca16
Aiyaz Mostofa <aiyaz5040@gmail.com>
2026-06-04 16:19:28 -0500

    Removed setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo usermod --shell /usr/bin/fish "$USER"
-sudo mkdir -p /var/nix
-sudo chown -v -R "$USER:$USER" /var/nix
-if [ ! -e "$HOME/.dotfiles" ]; then
-    ln -s "$(brew --repository aiyazmostofa/dotfiles)" "$HOME/.dotfiles"
-fi
-./sync.sh
--- sync.sh
+++ sync.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+if [ "$SHELL" != "/usr/bin/fish" ]; then
+    sudo usermod --shell /usr/bin/fish "$USER"
+fi
+if [ ! -e /var/nix ]; then
+    sudo mkdir -p /var/nix
+    sudo chown -v -R "$USER:$USER" /var/nix
+fi
+if [ ! -e "$HOME/.dotfiles" ]; then
+    ln -s "$(brew --repository aiyazmostofa/dotfiles)" "$HOME/.dotfiles"
+fi
 PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 brew bundle --file=./Brewfile
 stow --no-folding -t ~/.config .
